c335117c3d89e18219eeb43b4c4539f29bbeeb44
Shah <shah.dev@outlook.in>
2022-11-13 19:38:46 +0530

    lcm of two number - using formula

--- mathematics/lcm.cpp
+++ mathematics/lcm.cpp
@@ -14,8 +14,23 @@ int lcm (int num1, int num2){
     return result;
     // Time complexity: O(num1 * num2 - max(num1,num2))
 }
+// -----------------------------------------------------------
+int gcd (int num1 , int num2){
+        if (num2  == 0){
+            return num1;
+        }
+
+        return gcd(num2, num1 % num2);
+}
+
+int lcm2 (int num1, int num2){
+    // Formula => a * b = gcd(a,b) * lcm(a,b)
+    return (num1 * num2) / gcd(num1 , num2);
+    // Time complexity: O(log(min(num1,num2)))
+}
 
 int main()
 {
-    cout << lcm(7,13) << endl;
+    cout << lcm(12,24) << endl;
+    cout << lcm2(0,20) << endl;
 }
